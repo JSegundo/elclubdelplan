fb9accdb19dad82f7013902d940e2cfdc19def43
segundo <segujuan27@hotmail.com>
2022-03-24 11:53:19 -0300

    rama que no anda nada. intente config rn elems y r navigation

--- clubdelplan/App.js
+++ clubdelplan/App.js
@@ -1,19 +1,55 @@
-
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import {Text, View, ScrollView, Image, StyleSheet} from 'react-native';
+
+import HomeScreen from './src/screens/HomeScreen.jsx';
+import CatalogScreen from './src/screens/CatalogScreen.jsx';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>El plan del club hola hola</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="CatalogScreen" component={CatalogScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    backgroundColor: '#a52a2a',
+  },
+  title: {
+    color: 'white',
+    fontSize: 22,
+  },
+  imagen: {
+    width: 200,
+    height: 200,
+  },
+  input: {
+    borderWidth: 1,
+    width: 180,
+  },
+  Pressable: {
+    borderColor: 'blue',
+    backgroundColor: 'magenta',
+    padding: 20,
+  },
+  containerInput: {
+    width: '80%',
+    // flex: 1,
+    // alignItems: 'center',
+    borderColor: 'blue',
+    paddingHorizontal: 30,
+  },
+});
 
 export default App;
---
+++ clubdelplan/src/screens/CatalogScreen.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const CatalogScreen = () => {
+  return (
+    <View>
+      <Text>Catalog Screen</Text>
+    </View>
+  );
+};
+
+export default CatalogScreen;
