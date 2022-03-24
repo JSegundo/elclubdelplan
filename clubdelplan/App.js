58972071cfe8bf049ad2cd86291c81aff1d1cadd
segundo <segujuan27@hotmail.com>
2022-03-24 16:23:12 -0300

    bottom navigation created

--- clubdelplan/App.js
+++ clubdelplan/App.js
@@ -4,52 +4,18 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Text, View, ScrollView, Image, StyleSheet} from 'react-native';
 
-import HomeScreen from './src/screens/HomeScreen.jsx';
-import CatalogScreen from './src/screens/CatalogScreen.jsx';
+import Tabs from './src/navigation/Tabs.js';
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="CatalogScreen" component={CatalogScreen} />
-      </Stack.Navigator>
+      <Tabs />
     </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 100,
-    backgroundColor: '#a52a2a',
-  },
-  title: {
-    color: 'white',
-    fontSize: 22,
-  },
-  imagen: {
-    width: 200,
-    height: 200,
-  },
-  input: {
-    borderWidth: 1,
-    width: 180,
-  },
-  Pressable: {
-    borderColor: 'blue',
-    backgroundColor: 'magenta',
-    padding: 20,
-  },
-  containerInput: {
-    width: '80%',
-    // flex: 1,
-    // alignItems: 'center',
-    borderColor: 'blue',
-    paddingHorizontal: 30,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ clubdelplan/src/screens/NewPlanScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const NewPlanScreen = () => {
+  return (
+    <View>
+      <Text>NewPlanScreen</Text>
+    </View>
+  );
+};
+
+export default NewPlanScreen;
