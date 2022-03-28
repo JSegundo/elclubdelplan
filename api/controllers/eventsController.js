d82bb5a3e77211dfdbb0d60d504a7a173493f5dc
Mateo Buraschi <buraschi.mateo@gmail.com>
2022-03-28 14:08:13 -0300

    ultimo

--- api/controllers/eventsController.js
+++ api/controllers/eventsController.js
@@ -1,36 +1,35 @@
 const EventsService = require("../services/eventsServices");
 
-
-class EventsController{
-  static async getAllEvents(req, res, next){
+class EventsController {
+  static async getAllEvents(req, res, next) {
     const events = await EventsService.serviceGetAllEvents(req, next);
     return events ? res.status(200).json(events) : res.sendStatus(404);
-  };
+  }
 
- static async getEvent(req, res, next){
+  static async getEvent(req, res, next) {
     const event = await EventsService.serviceGetEvent(req, next);
     return event ? res.status(200).json(event) : res.sendStatus(404);
-  };
+  }
 
- static async eventByCategory(req, res, next){
+  static async eventByCategory(req, res, next) {
     const event = await EventsService.serviceEventByCategory(req, next);
     return res.send(event);
-  };
-  static async  updateProduct(req, res, next){
+  }
+  static async updateProduct(req, res, next) {
     const event = await EventsService.serviceUpdateEvent(req, next);
     return res.status(201).json(event);
-  };
+  }
 
-  static async addEvent(req, res, next){
+  static async addEvent(req, res, next) {
     const event = await EventsService.serviceAddEvent(req, next);
     return res.status(201).send(event);
-  };
-  
-  static async deleteEvent(req, res, next){
+  }
+
+  static async deleteEvent(req, res, next) {
     const result = await EventsService.serviceDeleteEvent(req, next);
     result
       ? res.status(204).send([])
       : res.status(404).send("The event you want to delete doesn't exist.");
-  };
+  }
 }
-module.exports= EventsController;
+module.exports = EventsController;
--- api/services/eventsServices.js
+++ api/services/eventsServices.js
@@ -1,57 +1,55 @@
 const { Events } = require("../models"); // preguntar como se exportan los modelos en mongo
 
-
-class EventsServices{
-  static async serviceGetAllEvents(req, next){
+class EventsServices {
+  static async serviceGetAllEvents(req, next) {
     try {
-        //METODO GETALL
+      //METODO GETALL
     } catch (err) {
       next(err);
     }
-    };
-  
-    static async serviceGetEvents(req, next){
-      try {
-          //METODO FINDONE
-      } catch (err) {
-        console.log(err);
-        next(err);
-      }
-    };
-    
-   static async serviceEventByCategory(req, next){
-      try {
-          //METODO GET POR CATEGORIA
-      } catch (err) {
-        next(err);
-      }
-    };
-    
-    
-  static async serviceUpdateEvent(req, next){
-      try {
-          //METODO UPDATE
-      } catch (err) {
-        next(err);
-      }
-    };
-    
-    static async serviceAddEvent(req, next){
-      try {
-          //METODO POST
-      } catch (err) {
-        console.log(err);
-        next(err);
-      }
-    };
-    
-   static async serviceDeleteEvent(req, next){
-      try {
-          //METODO DELETE
-      } catch (err) {
-        next(err);
-      }
-    };
+  }
+
+  static async serviceGetEvents(req, next) {
+    try {
+      //METODO FINDONE
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+
+  static async serviceEventByCategory(req, next) {
+    try {
+      //METODO GET POR CATEGORIA
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async serviceUpdateEvent(req, next) {
+    try {
+      //METODO UPDATE
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async serviceAddEvent(req, next) {
+    try {
+      //METODO POST
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+
+  static async serviceDeleteEvent(req, next) {
+    try {
+      //METODO DELETE
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
-module.exports= EventsServices;
+module.exports = EventsServices;
