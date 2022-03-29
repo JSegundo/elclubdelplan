ff54f29f951b5d27202ce0a97d32e17d48092821
matiob <buraschi.mateo@gmail.com>
2022-03-29 16:14:01 -0300

    services of users

--- api/services/usersServices.js
+++ api/services/usersServices.js
@@ -1,53 +1,63 @@
 const { Users } = require("../models"); // preguntar como se exportan los modelos en mongo
 
-class UsersService{
+class UsersService {
 
-  static async serviceResgisterUser(req){
+  static async serviceResgisterUser(req) {
     try {
-        //METODO REGISTER
-    } catch (err) {
-      console.log("err->", err);
-    }
-  };
-
-  static async serviceLogin(req){
-    try {
-        //METODO LOGIN
+      //FALTAN LAS VALIDACIONES
+      const newUser = new User(req.body);
+      const user = await newUser.save();
+      return user
     } catch (err) {
       console.log(err);
     }
   };
 
-  static async serviceLogout(req){
+  static async serviceLogin(req) {
+    try {
+      const user = await Users.find({ email: req.body.email });
+      //FALTA AVERIGUAR COMO FILTRAR DATOS PARA NO DEVOLVER PASS Y SALT
+      return user;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /*static async serviceLogout(req){
     try {
         //METODO LOGOUT
     } catch (err) {
       console.log(err);
     }
-  };
-  
-  static async serviceGetMe(req){
+  };*/
+
+  static async serviceGetMe(req) {
     try {
-        //METODO USUARIO LOGUEADO
+      return req.user ? req.user : null;
     } catch (err) {
       console.log(err);
-    }      
+    }
   };
 
-  static async serviceEditUser(req, next){
+  static async serviceEditUser(req, next) {
     try {
-        //METODO EDITAR USUARIO
+      const { id } = req.params;
+      const oldUser = await Users.findByIdAndUpdate(id, req.body);
+      // REVISAR QUE DEBERIA DEVOLVER ESTE METODO
+      return 1;
     } catch (err) {
       next(err);
     }
   };
-  
- static async serviceGetOneUser(req, next){
+
+  static async serviceGetOneUser(req, next) {
     try {
-        //METODO FINDONE
+      const { id } = req.params;
+      const user = await Users.findById(id);
+      return user;
     } catch (err) {
       next(err);
     }
   };
 }
-module.exports= UsersService;
+module.exports = UsersService;
