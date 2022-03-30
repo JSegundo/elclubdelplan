a55d98daa48a22b3ea6ac27ea6d23931d7a4da33
matiob <buraschi.mateo@gmail.com>
2022-03-30 20:44:57 -0300

    routes-controllers-services

---
+++ api/routes/categories.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const CategoriesController = require("../controllers/categoriesControllers"); 
+//RUTA PARA BUSCAR CATEGORIAS
+router.get("/", CategoriesController.getAllCategory);
+//RUTA PARA BUSCAR UNA CATERGORIA
+router.get("/:id", CategoriesController.getOneCategory);
+
+module.exports = router;
