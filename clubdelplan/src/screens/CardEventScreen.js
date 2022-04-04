3ab7a2493bd1fffc88fdab2a7cee5729cc0085d5
DelfinaRuth <delfiruthv@gmail.com>
2022-04-04 13:13:07 -0300

    branchlunes

---
+++ clubdelplan/src/screens/CardEventScreen.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const cardEvent = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+
+export default cardEvent;
